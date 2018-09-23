fab72df461c9d552822857c10ffb57ca8c0c2138
Rafael Santiago <rmxsantiago@gmail.com>
2018-09-23 11:06:40 +0100

    Initial project.

---
+++ iOS/I am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Rafael Santiago on 23/09/2018.
+//  Copyright © 2018 Rafael Santiago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
